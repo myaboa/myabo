ab65aae8925eed6065a731ef69e58b30fef4aac5
abo <My_ABo@163.com>
2023-05-20 16:17:00 +0800

    提交一个文件

---
+++ src/com/abo/spring/SpringConfig.java
@@ -0,0 +1,9 @@
+package com.abo.spring;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.abo.spring")
+public class SpringConfig {
+}
